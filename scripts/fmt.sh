c504dd8c713210a04e5ef23d6cbecdfcd8e4bbf2
Remus76 <75799128+Remus76@users.noreply.github.com>
2024-11-03 07:22:04 +0100

    Build fmt

---
+++ scripts/fmt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+VERSION=10.2.1
+
+curl -L -o fmt.zip "https://github.com/fmtlib/fmt/releases/download/$VERSION/fmt-$VERSION.zip"
+7z x fmt.zip
+
+mkdir build
+pushd build
+
+cmake "../fmt-$VERSION" \
+    -DFMT_DOC=OFF \
+    -DFMT_TEST=OFF \
+    $CMAKE_CONFIGURE_ARGS
+
+cmake --build . $CMAKE_BUILD_ARGS
+cmake --install . $CMAKE_BUILD_ARGS
+
+popd
+
+license fmt "fmt-$VERSION/LICENSE" MIT
